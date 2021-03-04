50d8933d88a61b5c2e5e613da9a162a94ce6f62f
andreluas <andreluas.k@gmail.com>
2021-03-04 20:45:38 -0300

    #1040 - Média :heavy_check_mark:

--- 01 - iniciante/URI_1040.java
+++ 01 - iniciante/URI_1040.java
@@ -1,33 +1,34 @@
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class URI_1040 {
 	
 	public static void main(String[] args) throws IOException {
-		Scanner leitor = new Scanner(System.in);
-		DecimalFormat df = new DecimalFormat("0.0");
-		double N1 = leitor.nextDouble();
-		double N2 = leitor.nextDouble();
-		double N3 = leitor.nextDouble();
-		double N4 = leitor.nextDouble();
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		double N1 = input.nextDouble();
+		double N2 = input.nextDouble();
+		double N3 = input.nextDouble();
+		double N4 = input.nextDouble();
         double MEDIA = ((N1*2) + (N2*3) + (N3*4) + (N4*1)) / 10;
-		System.out.println("Media: " + df.format(MEDIA));
+		System.out.printf("Media: %.1f%n", MEDIA);
         if (MEDIA >= 7) {
         	System.out.println("Aluno aprovado.");
         } else if (MEDIA < 7 && MEDIA >= 5) {
         	System.out.println("Aluno em exame.");
-        	double NExame = leitor.nextDouble();
-			System.out.println("Nota do exame: " + df.format(NExame));
+        	double NExame = input.nextDouble();
+			System.out.printf("Nota do exame: %.1f%n", NExame);
         	MEDIA = (MEDIA + NExame) / 2;
         	if (MEDIA >= 5) {
             	System.out.println("Aluno aprovado.");
         	} else {
             	System.out.println("Aluno reprovado.");
         	}
-			System.out.println("Media final: " + df.format(MEDIA));
+			System.out.printf("Media final: %.1f%n", MEDIA);
         } else {
         	System.out.println("Aluno reprovado.");
         }
+		input.close();
 	}
 }
